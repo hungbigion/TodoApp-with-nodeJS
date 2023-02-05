5db48749e02228fff9024b8bce4600ee86307c82
nguyen huu hung <www.nguyenhuuhung@gmail.com>
2023-02-05 20:54:25 +0700

    CRUD task todo

--- routers/todo.js
+++ routers/todo.js
@@ -28,4 +28,74 @@ router.post('/',auth, async (req, res, next) => {
     }
 });
 
+// desc fetch all todos
+// method get
+router.get('/', auth, async (req, res, next) => {
+    try {
+        const todo = await Todo.find({
+            user: req.user.id,
+            finished: false
+        })
+        if (!todo){
+            return res.status(400).json({ success: false, msg: 'Error'})
+        }
+        res.status(200).json({
+            success: true,count: todo.length,todos: todo, msg:'Success'})
+    } catch (error) {
+        next(error);
+    }
+});
+// desc fetch all todo finished
+router.get('/finished', auth, async (req, res, next) => {
+    try {
+        const todo = await Todo.find({
+            user: req.user.id,
+            finished: true
+        })
+        if (!todo){
+            return res.status(400).json({ success: false, msg: 'Error'})
+        }
+        res.status(200).json({
+            success: true,count: todo.length,todos: todo, msg:'Success'})
+    } catch (error) {
+        next(error);
+    }
+});
+
+// update task method put
+router.put('/:id', async(req, res, next) => {
+    try {
+        let todo  = await Todo.findById(req.params.id);
+        if (!todo) {
+            return res.status(400).json({success: false, msg: 'Task not exist'});
+        }
+        todo = await Todo.findByIdAndUpdate(req.params.id, req.body,{
+            new: true,
+            runValidators: true
+        });
+        return res.status(200).json({
+            success: true,todo: todo, msg: 'Success update'});
+
+    } catch (error) {
+        next(error);
+    }
+});
+
+// desc delete task
+router.delete('/:id', async(req, res, next) => {
+    try {
+        let todo  = await Todo.findById(req.params.id);
+        if (!todo) {
+            return res.status(400).json({success: false, msg: 'Task not exist'});
+        }
+        todo = await Todo.findByIdAndDelete(req.params.id);
+
+        return res.status(200).json({
+            success: true, msg: 'Deleted'});
+
+    } catch (error) {
+        next(error);
+    }
+});
+
 module.exports = router;
